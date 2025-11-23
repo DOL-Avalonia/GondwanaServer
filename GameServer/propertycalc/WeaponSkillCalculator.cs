a9813e83e5aa03e011491b2676e8a3bf19864c0d
Amber Ehrlich <miuna.oshino@gmail.com>
2025-11-23 16:38:51 +0100

    misc: tidy up WeaponSkillCalculator

--- GameServer/propertycalc/WeaponSkillCalculator.cs
+++ GameServer/propertycalc/WeaponSkillCalculator.cs
@@ -6,12 +6,20 @@ namespace DOL.GS.PropertyCalc
     {
         public override int CalcValue(GameLiving living, eProperty property)
         {
-            double percent = 100
-            + living.BaseBuffBonusCategory[(int)property] // enchance the weaponskill
-            + living.SpecBuffBonusCategory[(int)property] // enchance the weaponskill
-                                                          //hotfix for poisons where both debuff components have same value
-            - (int)Math.Abs(living.DebuffCategory[(int)property] / 5.4) // reduce
-            + living.ItemBonus[(int)property];
+            double percent = 100;
+
+            percent += living.ItemBonus[(int)property];
+            percent += living.BaseBuffBonusCategory[(int)property]; // enchance the weaponskill
+            percent += living.SpecBuffBonusCategory[(int)property]; // enchance the weaponskill
+            //hotfix for poisons where both debuff components have same value
+            percent -= (int)Math.Abs(living.DebuffCategory[(int)property] / 5.4); // reduce
+            return (int)Math.Max(1, percent);
+        }
+
+        /// <inheritdoc />
+        public override int CalcValueBase(GameLiving living, eProperty property)
+        {
+            double percent = 100 + living.ItemBonus[(int)property];
             return (int)Math.Max(1, percent);
         }
     }
